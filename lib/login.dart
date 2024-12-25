ab9834e0c16c69e7c501f415dc3db86fb5ebaf2e
BhojrajCSE21 <bhojrajnarware@gmail.com>
2024-12-25 17:02:00 +0530

    Login Complete

--- lib/login.dart
+++ lib/login.dart
@@ -11,50 +11,86 @@ class _MyLoginState extends State<MyLogin> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-      image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
-      
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Stack(
           children: [
             Container(
-
-              padding: EdgeInsets.only(left: 30, top: 170),
-              child: Text("Login Page", style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),),),
+              padding: EdgeInsets.only(left: 30, top: 390),
+              child: Text(
+                "Welcome to \nLogin Page",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
             SingleChildScrollView(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, left: 30, right: 30),
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.54,
+                  left: 30,
+                  right: 30),
               child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey.shade200,
-                    filled: true,
-                  hintText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20)))),
-                SizedBox(height: 25,),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey.shade200,
-                    filled: true,
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20)))),
-              ],
+                children: [
+                  TextField(
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade300,
+                          filled: true,
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)))),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade300,
+                          filled: true,
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)))),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween, // Adjust spacing
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.blue,
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_forward),
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'home');
+                          },
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             )
-
           ],
         ),
       ),
-      );
-
+    );
   }
 }
